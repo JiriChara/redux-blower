e6ab930f261ca0d6b5d581f2eb95e0d8f70c04e6
Jiri Chara <me@jirichara.com>
2016-04-22 12:28:46 +0200

    Fix files path in karma config

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function (config) {
     // list of files / patterns to load in the browser
     files: [
       'index.js',
-      'lib/**/*.js',
+      'src/**/*.js',
       'spec/**/*.spec.js'
     ],
 
@@ -27,7 +27,7 @@ module.exports = function (config) {
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
       'index.js': ['browserify'],
-      'lib/**/*.js': ['browserify'],
+      'src/**/*.js': ['browserify'],
       'spec/**/*.spec.js': ['browserify']
     },
 
